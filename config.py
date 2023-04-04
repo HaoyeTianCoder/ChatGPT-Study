ab1ec829f7c7c44a088723564b7bbcc39b79e648
williamlus <luwilam29@gmail.com>
2023-04-04 15:33:34 +0800

    update config

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ class Config:
     def __init__(self):
         # replace self.path with the path to data or data_des
         self.path = '/Users/haoye.tian/Documents/University/project/refactory/data/'
-        self.generation_path = 'C:/Users/luwil/OneDrive - HKUST Connect/Desktop/UST/ChatGPT/leetcode2/code_generation_dataset'
+        
+        # replace self.generation_path with the path to code_generation_dataset
+        self.generation_path = '/mnt/c/Users/luwil/OneDrive - HKUST Connect/Desktop/UST/ChatGPT/leetcode2/code_generation_dataset'
+        
+        # update self.openai_api_key and self.openai_organization with your own key and organization
         self.openai_api_key = ''
         self.openai_organization = ''
